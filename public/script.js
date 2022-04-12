e42a1cd0df027bdb49d71d0e56462c29e1b5ef86
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2022-04-11 17:28:43 -0700

    Added logic to automatically switch api urls from development to production

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
 const feedURL = "https://www.patreon.com/rss/dungeonsanddads?auth=Te1pL8_ENX5yUKVVz5LajSpQVcsw86-7";
-const serverURL = "http://localhost:5000" //"https://rss-podcast-player.herokuapp.com"
+let serverURL;
+if (window.location.hostname.includes("localhost"))
+  serverURL = "http://localhost:5000"
+else {
+  serverURL = "https://rss-podcast-player.herokuapp.com"
+}
 
 const parent = document.getElementById("podcasts");
 let podcasts = [];
 let queue = [];
+
+
 
 fetch(serverURL + "/podcasts").then((response)=>{
   response.json().then(res => {
